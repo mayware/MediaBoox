0b91d2b7a2276f14fc5d9e61b6f9414689be79ae
tofa93 <tofig.valiyev1993@gmail.com>
2023-01-24 20:28:52 +0100

    changed content, add dynamic year display

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ function sideToggler() {
     document.querySelector('[data-sidebar]').classList.toggle('open');
     document.querySelector('.container').classList.toggle('open');
 }
+
+function blockExpander() {
+    document.querySelector('.show-more').classList.toggle('open');
+    document.querySelector('.show-less').classList.toggle('open');
+    document.querySelector('.arrow-icon-down').classList.toggle('close');
+    document.querySelector('.arrow-icon-up').classList.toggle('open');
+    document.querySelector('.banners-box').classList.toggle('expand');
+}
+
+let yearDate = document.querySelector('.year-date');
+yearDate.innerHTML = new Date().getFullYear() + " ";
